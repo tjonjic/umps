ba93935b6a441564f9f275f6a53ca0e8df0775c7
Tomislav Jonjic <jonjic@cs.unibo.it>
2011-06-04 20:12:09 +0200

    Remove unused bios break code

--- src/support/h/bios_srv.h
+++ src/support/h/bios_srv.h
@@ -2,14 +2,13 @@
  * uMPS BIOS related constants
  */
 
-#ifndef BIOS_H
-#define BIOS_H
+#ifndef BIOS_SRV_H
+#define BIOS_SRV_H
 
 /* FIXME: Are all of these safe to use (abuse?) as break codes? */
 #define BIOS_SRV_FORK        0
 #define BIOS_SRV_LDST        1
 #define BIOS_SRV_PANIC       2
 #define BIOS_SRV_HALT        3
-#define BIOS_SRV_START_CPU   4
 
-#endif /* !defined(BIOS_H) */
+#endif /* !defined(BIOS_SRV_H) */
